469ab534532071d1260ce5886bb2242193a95589
Akuna444 <aklilsolomon3@gmail.com>
2022-12-28 01:37:28 +0300

    update header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import {Fragment} from "react";
+import { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
@@ -7,8 +7,8 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
-        <HeaderCartButton  onClick={props.onShowCart}/>
+        <h1>Ethiopian Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicous foods" />
